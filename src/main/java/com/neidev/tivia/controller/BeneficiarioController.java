5e985bd5b4ffcc5428cb15b003f410bcfcad4b27
Nathan <nathanbarrosnt@gmail.com>
2023-11-14 10:26:14 -0300

    feat: beneficiario & documentos endpoints working

--- src/main/java/com/neidev/tivia/controller/BeneficiarioController.java
+++ src/main/java/com/neidev/tivia/controller/BeneficiarioController.java
@@ -1,10 +1,44 @@
 package com.neidev.tivia.controller;
 
+import com.neidev.tivia.domain.core.json.beneficiario.BeneficiarioCadastroForm;
+import com.neidev.tivia.domain.core.json.beneficiario.BeneficiarioForm;
+import com.neidev.tivia.domain.core.model.Beneficiario;
+import com.neidev.tivia.service.impl.BeneficiarioService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/beneficiario")
 public class BeneficiarioController {
 
+    private BeneficiarioService beneficiarioService;
 
+    public BeneficiarioController(BeneficiarioService beneficiarioService) {
+        this.beneficiarioService = beneficiarioService;
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<BeneficiarioForm> cadastrarBeneficiario(@RequestBody BeneficiarioCadastroForm data) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(beneficiarioService.cadastrarBeneficiario(data));
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<BeneficiarioForm>> listarBeneficiarios() {
+        return ResponseEntity.status(HttpStatus.OK).body(beneficiarioService.listarBeneficiarios());
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<BeneficiarioForm> atualizarBeneficario(@RequestBody Beneficiario data, @PathVariable String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(beneficiarioService.atualizarBeneficario(data, id));
+    }
+
+    @DeleteMapping("/excluir/{id}")
+    public ResponseEntity removerBeneficiario(@PathVariable String id) {
+        beneficiarioService.removerBeneficiario(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- src/main/java/com/neidev/tivia/controller/DocumentoController.java
+++ src/main/java/com/neidev/tivia/controller/DocumentoController.java
@@ -1,9 +1,30 @@
 package com.neidev.tivia.controller;
 
+import com.neidev.tivia.domain.core.json.documento.DocumentoForm;
+import com.neidev.tivia.service.impl.DocumentoService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.Document;
+import java.util.List;
 
 @RestController
 @RequestMapping("/documento")
 public class DocumentoController {
+
+    private DocumentoService documentoService;
+
+    public DocumentoController(DocumentoService documentoService) {
+        this.documentoService = documentoService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<DocumentoForm>> listarDocumentosBeneficiario(@PathVariable String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(documentoService.listarDocumentosBeneficiario(id));
+    }
 }
