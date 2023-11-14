017bbff179d217b696880695cd013068d2ccae25
Nathan <nathanbarrosnt@gmail.com>
2023-11-14 07:33:16 -0300

    release: base files

---
+++ src/main/java/com/neidev/tivia/TiviaApplication.java
@@ -0,0 +1,13 @@
+package com.neidev.tivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiviaApplication.class, args);
+	}
+
+}
